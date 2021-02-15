11477cac2d3a10ffdf9fd8e54513b68c9a179fd3
fak3er <48171752+fak3er@users.noreply.github.com>
2021-02-15 13:56:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ window.onload=()=>{
     minute=document.querySelector('.minutes')
     hour=document.querySelector('.hours')
     hours=document.querySelector('.container').children
-    console.log(hours)
+    button=document.querySelector('.mvt')
+    let fluid=false
     for(let i=1;i<=hours.length;i++){
         hours[i-1].style.transform="rotateZ("+30*(i)+"deg)"
     }
-    
+    button.onclick=()=>{
+        fluid=!fluid
+    }
+       
     function renderTime(){
         hour.style.transform="rotate("+(new Date(Date.now())).getHours()%12*((360/12))+"deg)"
         minute.style.transform="rotate("+(new Date(Date.now())).getMinutes()*(360/60)+"deg)"
-        second.style.transform="rotate("+(new Date(Date.now())).getSeconds()*(360/60)+"deg)"
-       setTimeout(renderTime,1000)
+        if(!fluid)
+            {
+                second.style.transform="rotate("+(new Date(Date.now())).getSeconds()*(360/60)+"deg)"
+                setTimeout(renderTime,1000)
+            }
+        else{
+            second.style.transform="rotate("+((((new Date(Date.now())).getSeconds()*1000)+((new Date(Date.now())).getMilliseconds()))/100)*(360/600)+"deg)"
+            setTimeout(renderTime,100)
+        }    
         }
-
-
-        renderTime()
+    renderTime()
 }
