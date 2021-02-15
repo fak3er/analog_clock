0229df1d9b794a7230b5040c3785270508d2cff8
fak3er <48171752+fak3er@users.noreply.github.com>
2021-02-15 21:17:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ window.onload=()=>{
     hour=document.querySelector('.hours')
     hours=document.querySelector('.container').children
     button=document.querySelector('.mvt')
+    tic=document.querySelector('audio')
+    play=document.querySelector('.sound')
     let fluid=false
+    let audio=false
     for(let i=1;i<=hours.length;i++){
         hours[i-1].style.transform="rotateZ("+30*(i)+"deg)"
     }
     button.onclick=()=>{
         fluid=!fluid
     }
-       
+    play.onclick=()=>{
+        audio=!audio
+    }   
     function renderTime(){
         hour.style.transform="rotate("+(new Date(Date.now())).getHours()%12*((360/12))+"deg)"
         minute.style.transform="rotate("+(new Date(Date.now())).getMinutes()*(360/60)+"deg)"
         if(!fluid)
             {
+                if(audio)
+               setTimeout( ()=>tic.play(),800)
                 second.style.transform="rotate("+(new Date(Date.now())).getSeconds()*(360/60)+"deg)"
                 setTimeout(renderTime,1000)
             }
